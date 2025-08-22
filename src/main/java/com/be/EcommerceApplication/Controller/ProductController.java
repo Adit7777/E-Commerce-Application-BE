88916d19b5282ac3f2aa34af1bd05f75b101face
adit777 <adit.patange@gmail.com>
2025-08-22 09:04:17 +0530

    changes in properties for port and added list method to project

--- src/main/java/com/be/EcommerceApplication/Controller/ProductController.java
+++ src/main/java/com/be/EcommerceApplication/Controller/ProductController.java
@@ -2,11 +2,13 @@ package com.be.EcommerceApplication.Controller;
 
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,15 @@ public class ProductController {
 		product.setCreatedDate(LocalDateTime.now());
 		product.setStatus("Active");
 		Product result  = productService.save(product);
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(result);
+	}
+	
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<Product>> allProduct(){
+		
+		List<Product> result  = productService.getAll();
 		
 		return ResponseEntity.status(HttpStatus.CREATED).body(result);
 	}
--- src/main/java/com/be/EcommerceApplication/Service/ProductService.java
+++ src/main/java/com/be/EcommerceApplication/Service/ProductService.java
@@ -1,8 +1,12 @@
 package com.be.EcommerceApplication.Service;
 
+import java.util.List;
+
 import com.be.EcommerceApplication.Model.Product;
 
 public interface ProductService {
 
 	public Product save(Product product);
+
+	public List<Product> getAll();
 }
